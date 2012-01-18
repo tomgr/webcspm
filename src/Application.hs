250feba33d3b3a798550108b84f08ed955900c30
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2012-01-18 15:34:21 +0000

    Add the ability to kill the server remotely (to allow for updates)

--- src/Application.hs
+++ src/Application.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Application where
     
+import Control.Concurrent (ThreadId)
 import Data.Lens.Template
 import Snap.Snaplet
 import Snap.Snaplet.Heist
 
 data App = App { 
-        _heist :: Snaplet (Heist App)
+        _heist :: Snaplet (Heist App),
+        _threadId :: ThreadId
     }
 
 type AppHandler = Handler App App
